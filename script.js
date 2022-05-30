d73f37f05c704401cde3ce53dc3b2ae7e660b909
mirekbartunek <mucus.hooide@gmail.com>
2022-05-30 13:47:59 +0200

    work almost done

---
+++ script.js
@@ -0,0 +1,6 @@
+function randomStatement(){
+    const randomStatements = ["I don’t really like JS", "Primitive data type", "forced to develop java", "i don’t know why"]
+    for i in randomStatements {
+
+    }
+}
