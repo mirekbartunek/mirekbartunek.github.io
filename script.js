64b7ca8de29429535d732b5b7d6169daf0d12ceb
mirekbartunek <mucus.hooide@gmail.com>
2022-06-01 18:53:59 +0200

    sdfh

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-function randomStatement(){
-    const randomStatements = ["I don’t really like JS", "Primitive data type", "forced to develop java", "i don’t know why"]
-    for i in randomStatements {
-
-    }
-}
+//
